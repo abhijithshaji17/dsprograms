3617b6f46f6f3bde71f4c2f34239824f1065deea
abhijithshaji17 <abhijithshaji700@gmail.com>
2026-07-14 11:28:50 +0530

    ds-lab1

--- 2.cpp
+++ 2.cpp
@@ -1,5 +1,28 @@
 // Template 2
 #include<iostream>
+#include<string>
 using namespace std;
 template <typename T>
-class calculator{}
+class calculator{
+    T num1, num2;
+    public:
+        calculator(T n1, T n2){
+            num1 = n1;
+            num2 = n2;
+        }
+        T add(){
+            return num1+num2;
+        }
+        T multiply(){
+            return num1*num2;
+        }
+};
+int main(){
+    calculator<int>ob1(3,4);
+    calculator<float>ob2(4.5,5.1);
+    calculator<string>ob3("Hai","Ben");
+    cout<<ob1.add()<<endl;
+    cout<<ob2.add()<<endl;
+    cout<<ob3.add()<<endl;
+    return 0;
+}
